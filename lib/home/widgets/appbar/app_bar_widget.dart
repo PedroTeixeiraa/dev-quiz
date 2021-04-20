920aaa5124bdc541f82c97c53a3a6de74352c0e4
Pedro Alves <pedroteixeiraalves007@gmail.com>
2021-04-19 21:50:25 -0300

    feat: create app bar home page

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,39 @@
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_images.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(TextSpan(
+                        text: "Olá, ",
+                        style: AppTextStyles.title,
+                        children: [
+                          TextSpan(
+                              text: "Pedro Alves!",
+                              style: AppTextStyles.titleBold)
+                        ])),
+                    Container(
+                      height: 58,
+                      width: 58,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/54821438?v=4"))),
+                    )
+                  ],
+                ),
+              ),
+            ));
+}
