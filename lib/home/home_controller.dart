a3bbd77a50bb0469763b701b33a28b5353dfdbf3
Pedro Alves <pedroteixeiraalves007@gmail.com>
2021-04-21 20:02:25 -0300

    feat: create home repository

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,7 +1,5 @@
-import 'package:DevQuiz/core/app_images.dart';
+import 'package:DevQuiz/home/home_repository.dart';
 import 'package:DevQuiz/home/widgets/home_state.dart';
-import 'package:DevQuiz/shared/models/answer_model.dart';
-import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
@@ -15,21 +13,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-
-    user = UserModel(
-      name: "Gabul DEV",
-      photoUrl: "https://avatars.githubusercontent.com/u/54821438?v=4",
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [];
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
