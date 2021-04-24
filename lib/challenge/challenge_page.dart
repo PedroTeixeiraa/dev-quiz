89c7e55a68e901e643f7f4192f547bee7f0121c7
Pedro Alves <pedroteixeiraalves007@gmail.com>
2021-04-24 15:02:36 -0300

    feat: answering quiz questions

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -29,6 +29,14 @@ class _ChallengePageState extends State<ChallengePage> {
     super.initState();
   }
 
+  void nextPage() {
+    if (controller.currentPage < widget.questions.length)
+      pageController.nextPage(
+        duration: Duration(milliseconds: 100),
+        curve: Curves.linear,
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,37 +67,40 @@ class _ChallengePageState extends State<ChallengePage> {
       body: PageView(
         physics: NeverScrollableScrollPhysics(),
         controller: pageController,
-        children: widget.questions.map((e) => QuizWidget(question: e)).toList(),
+        children: widget.questions
+            .map((e) => QuizWidget(
+                  question: e,
+                  onChange: nextPage,
+                ))
+            .toList(),
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: NextButtonWidget.white(
-                  label: "Pular",
-                  onTap: () {
-                    pageController.nextPage(
-                      duration: Duration(milliseconds: 100),
-                      curve: Curves.linear,
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                width: 7,
-              ),
-              Expanded(
-                child: NextButtonWidget.darkGreen(
-                  label: "Confirmar",
-                  onTap: () {},
-                ),
-              ),
-            ],
-          ),
+          child: ValueListenableBuilder<int>(
+              valueListenable: controller.currentPageNotifier,
+              builder: (context, value, _) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      if (value < widget.questions.length)
+                        Expanded(
+                          child: NextButtonWidget.white(
+                            label: "Pular",
+                            onTap: nextPage,
+                          ),
+                        ),
+                      if (value == widget.questions.length)
+                        Expanded(
+                          child: NextButtonWidget.darkGreen(
+                            label: "Confirmar",
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                    ],
+                  )),
         ),
       ),
     );
--- lib/challenge/widget/quiz/quiz_widget.dart
+++ lib/challenge/widget/quiz/quiz_widget.dart
@@ -1,15 +1,29 @@
 import 'package:DevQuiz/challenge/widget/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
-  const QuizWidget({Key? key, required this.question}) : super(key: key);
+  final VoidCallback onChange;
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
-    var question2 = question;
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: Column(
@@ -18,20 +32,24 @@ class QuizWidget extends StatelessWidget {
             height: 54,
           ),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          ...question2.answers
-              .map(
-                (e) => AnswerWidget(
-                  title: e.title,
-                  isRight: e.isRight,
-                ),
-              )
-              .toList(),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answer(i),
+              isSelected: indexSelected == i,
+              disabled: indexSelected != -1,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange());
+              },
+            ),
         ],
       ),
     );
